34e2cf9dcba59d9773c0f9897394672eec8ea0aa
Won Jun Bae <wonjun@hotmail.com>
2014-04-27 18:26:49 -0400

    player control + bullets

--- js/game.js
+++ js/game.js
@@ -48,16 +48,73 @@ function main() {
 
 // Game state
 var player = {
-    pos: [145, 440],
+    pos: [145, 430],
+    speed: 100,
+    boundary_x: [18, canvas.width - 42],
+    boundary_y: [100, canvas.height - 50],
     sprite: new Sprite('images/game/characters.png', [121, 185], [27, 30], [0,1,2])
 };
 
+var bullets = [];
+var bulletSpeed = 500;
+
 function update(delta) {
-	updateEntities(delta)
+    handleInput(delta);	
+	updateEntities(delta);
+}
+
+function handleInput(delta) {
+	
+	if(input.isDown('DOWN') || input.isDown('s')) {
+		player.pos[1] += player.speed * delta;
+		if(player.pos[1] > player.boundary_y[1]) {
+			player.pos[1] = player.boundary_y[1];
+		}
+	}
+
+	if(input.isDown('UP') || input.isDown('w')) {
+		player.pos[1] -= player.speed * delta;
+		if(player.pos[1] < player.boundary_y[0]) {
+			player.pos[1] = player.boundary_y[0];
+		}		
+	}
+
+    if(input.isDown('LEFT') || input.isDown('a')) {
+        player.pos[0] -= player.speed * delta;
+		if(player.pos[0] < player.boundary_x[0]) {
+			player.pos[0] = player.boundary_x[0];
+		}	 
+    }
+
+    if(input.isDown('RIGHT') || input.isDown('d')) {
+        player.pos[0] += player.speed * delta;
+     	if(player.pos[0] > player.boundary_x[1]) {
+			player.pos[0] = player.boundary_x[1];
+		}	   
+    }
+
+    if(input.isDown('SPACE')) {
+    	var x = player.pos[0] + 7;
+    	var y = player.pos[1] - 15;
+
+    	bullets.push({
+    		pos: [x, y],
+    		sprite: new Sprite('images/game/characters.png', [107,145], [7, 25], [0])
+    	});
+    }
 }
 
 function updateEntities(delta) {
 	player.sprite.update(delta);
+
+	for(var i = 0; i < bullets.length; i++) {
+		var bullet = bullets[i];
+		bullet.pos[1] -= bulletSpeed * delta;
+
+		if(bullet.pos[1] < -bullet.sprite.size[1]) {
+			bullets.splice(i, 1);
+		}
+	}
 }
 
 function render() {
@@ -69,17 +126,24 @@ function render() {
 		context.font = "32px unicorn bold";
 		context.textAlign = "left";
 		context.textBaseline = "top";
-		context.fillText(score, 155, 65);
+		context.fillText(score, 155, 50);
 	}
 
-	render_entity(player);
+	renderEntity(player);
+	renderEntities(bullets);
 }
 
-function render_entity(entity) {
+function renderEntity(entity) {
 	context.save();
     context.translate(entity.pos[0], entity.pos[1]);	
 	entity.sprite.render(context);
 	context.restore();
+}
+
+function renderEntities(list) {
+	for(var i = 0; i < list.length; i++) {
+		renderEntity(list[i]);
+	}
 }
 
 function init() {
